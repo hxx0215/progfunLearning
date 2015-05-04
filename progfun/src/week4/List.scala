905a5b194c183bebc3eb9ee03e3aa578ea6a7eaa
hxx0215 <hxx0215@126.com>
2015-05-04 20:30:41 +0800

    ex3

--- progfun/src/week4/List.scala
+++ progfun/src/week4/List.scala
@@ -1,23 +1,28 @@
 package week4
 
-trait List[T] {
+trait List[+T] {
 def isEmpty: Boolean
 def head: T
 def tail:List[T]
+def prepend [U>: T](elem: U): List[U] = new Cons(elem,this)
 }
 class Cons[T] (val head:T,val tail: List[T]) extends List[T]{
   def isEmpty = false
   override def toString = head+","+tail
 }
-class Nil[T] extends List[T]{
+object Nil extends List[Nothing]{
   def isEmpty = true
   def head = throw new NoSuchElementException("Nil.head")
   def tail = throw new NoSuchElementException("Nil.tail")
   override def toString = "over"
 }
 
-object List{
-  def apply[T](x: T, y: T) = new Cons(x,new Cons(y, new Nil))
-  def apply[T]() = new Nil
-  def apply[T](x: T) = new Cons(x,new Nil)
+//object List{
+//  def apply[T](x: T, y: T) = new Cons(x,new Cons(y, new Nil))
+//  def apply[T]() = new Nil
+//  def apply[T](x: T) = new Cons(x,new Nil)
+//}
+object test{
+  val x: List[String] = Nil
+  
 }
