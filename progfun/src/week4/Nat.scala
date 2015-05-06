fdd7569cab2561ee57c33051110a8aa08c7ec7cf
hxx0215 <hxx0215@126.com>
2015-05-06 22:39:46 +0800

    week4 ex4

--- progfun/src/week4/Nat.scala
+++ progfun/src/week4/Nat.scala
@@ -12,12 +12,13 @@ object Zero extends Nat{
    def isZero = true
    def predecessor = throw new Error("0.predecessor")
    def +(that: Nat)= that
-   def -(that: Nat) = 
+   def -(that: Nat) = if (that.isZero) this else throw new Error("negtive number")
 }
 
 
 class Succ(n: Nat) extends Nat{
   def isZero = false
   def predecessor = n
-  
+  def +(that: Nat) = new Succ(n + that)
+  def -(that: Nat) = if (that.isZero) this else n - that.predecessor
 }
